0e37ab6c82cb025eb0658eb0078894a7f4c334a5
lauren12133 <77604108+lauren12133@users.noreply.github.com>
2022-09-28 14:20:47 +0800

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -76,13 +76,74 @@ root_user(){
 }
 
 tcp_bbr(){
-	echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+  echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
   echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
   sysctl -p
   lsmod | grep bbr
   back2menu
 }
 
+tcp_up(){
+cat > '/etc/sysctl.conf' << EOF
+fs.file-max=1000000
+fs.inotify.max_user_instances=65536
+
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
+
+net.ipv6.conf.all.forwarding = 1
+net.ipv6.conf.default.forwarding = 1
+net.ipv6.conf.lo.forwarding = 1
+net.ipv6.conf.all.disable_ipv6 = 0
+net.ipv6.conf.default.disable_ipv6 = 0
+net.ipv6.conf.lo.disable_ipv6 = 0
+
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_retries1=3
+net.ipv4.tcp_retries2=5
+net.ipv4.tcp_orphan_retries=3
+net.ipv4.tcp_syn_retries=3
+net.ipv4.tcp_synack_retries=3
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_fin_timeout=15
+net.ipv4.tcp_max_tw_buckets=32768
+net.ipv4.tcp_max_syn_backlog=131072
+net.core.netdev_max_backlog=131072
+net.core.somaxconn=32768
+net.ipv4.tcp_notsent_lowat=16384
+net.ipv4.tcp_keepalive_time=300
+net.ipv4.tcp_keepalive_probes=3
+net.ipv4.tcp_keepalive_intvl=30
+net.ipv4.tcp_fastopen=3
+net.ipv4.tcp_autocorking=0
+net.ipv4.tcp_slow_start_after_idle=0
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_rfc1337=0
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_moderate_rcvbuf=1
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
+net.ipv4.tcp_rmem=4096 87380 33554432
+net.ipv4.tcp_wmem=4096 16384 33554432
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.ipv4.tcp_mem=262144 1048576 4194304
+net.ipv4.udp_mem=262144 1048576 4194304
+net.ipv4.tcp_congestion_control=bbr
+net.core.default_qdisc=fq
+net.ipv4.ping_group_range=0 2147483647
+EOF
+sysctl -p /etc/sysctl.conf > /dev/null
+  back2menu
+}
 
 acme_rg(){
 REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "fedora")
@@ -417,11 +478,35 @@ menu() {
         7) revoke_cert ;;
         8) renew_cert ;;
         9) switch_provider ;;
-        *) exit 1 ;;
+        *) back2menu ;;
     esac
 }
-
-menu
-back2menu
+    menu
 }
 
+menu(){
+	clear
+	red "=================================="
+	green "             cc tool              "
+	red "          cc liux一键运行脚本    "
+	echo "                           "
+	red "=================================="
+	echo "                           "
+	green "1. root/ssh登录/改密码/ssh端口"
+	green "2. 设置bbr"
+	green "3. tcp调优"
+        green "4. acme一键注册证书"
+        green "5. 卸载程序"
+	green "0. 退出"
+	echo "         "
+	read -p "请输入数字:" NumberInput
+	case "$NumberInput" in
+		1) get_speedtest ;;
+		2) run_speedtest ;;
+		3) 1run_speedtest ;;
+		4) server ;;
+                5) rm -rf /root/tool.sh && read -p "回车重置变量:" NumberInput ;;
+		0) exit 1 ;;
+	esac
+}
+menu
