c1809ec398e68ccccf03ea0b7f4eb167648102ac
lauren12133 <77604108+lauren12133@users.noreply.github.com>
2023-03-06 11:45:49 +0800

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -336,8 +336,8 @@ acme_standalone(){
     domainIP=$(curl -sm8 ipget.net/?ip="${domain}")
     
     if [[ $domainIP == $ipv6 ]]; then
-        bash ~/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-256 --renew-hook 
-	bash ~/.acme.sh/acme.sh --listen-v6 --install-cert -d ${domain} --key-file /root/key.pem --fullchain-file /root/cert.pem --ecc
+        ~/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-256 --renew-hook 
+	~/.acme.sh/acme.sh --listen-v6 --install-cert -d ${domain} --key-file /root/key.pem --fullchain-file /root/cert.pem --ecc
     fi
     if [[ $domainIP == $ipv4 ]]; then
         bash ~/.acme.sh/acme.sh --issue -d ${domain} --standalone -k ec-256 --renew-hook 
